04a69e199420da3a9eaa668cb952168c9b82b9ae
Herman Kamper <kamperh@gmail.com>
2019-06-21 16:40:14 +0100

    Updated paths

--- paths.py
+++ paths.py
@@ -1,5 +1,5 @@
-# gp_data_dir = "/group/corporapublic/global_phone/"
-# gp_alignments_dir = "/disk/scratch/v1hkampe/endgame/datasets/globalphone_alignments/"
-gp_data_dir = "/home/kamperh/endgame/datasets/globalphone/"
-gp_alignments_dir = "/home/kamperh/endgame/datasets/globalphone_alignments/"
+gp_data_dir = "/group/corporapublic/global_phone/"
+gp_alignments_dir = "/disk/scratch/v1hkampe/endgame/datasets/globalphone_alignments/"
+# gp_data_dir = "/home/kamperh/endgame/datasets/globalphone/"
+# gp_alignments_dir = "/home/kamperh/endgame/datasets/globalphone_alignments/"
 
